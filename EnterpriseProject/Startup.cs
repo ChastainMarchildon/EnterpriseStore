e6e7539e7fea58beb7c37ea9005fc2f74d47716e
ChastainMarchildon <200214129@student.georgianc.on.ca>
2018-01-26 14:36:55 -0500

    first commit

---
+++ EnterpriseProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EnterpriseProject.Startup))]
+namespace EnterpriseProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
