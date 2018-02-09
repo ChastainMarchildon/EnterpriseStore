02e8a4f2714ab0e6a6bbc0c798751cad01bfa071
ChastainMarchildon <200214129@student.georgianc.on.ca>
2018-02-09 13:29:06 -0500

    init

---
+++ EnterpriseProject/Controllers/StoreController.cs
@@ -0,0 +1,48 @@
+﻿using EnterpriseProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EnterpriseProject.Controllers
+{
+    public class StoreController : Controller
+    {
+        // GET: Store
+        public ActionResult Index()
+        {
+            //create a mock list genre object
+            var genres = new List<Genre>();
+
+            for (int i = 1; i<11; i++)
+            {
+                genres.Add(new Genre { Name = "Genre" + i.ToString() });
+            }
+            //ViewBag.Genres = genres;
+            return View(genres);
+
+        }
+
+        //GET: Store/Details?genre=[value]
+        public ActionResult Details(string genre)
+        {
+            //check for an empty genre
+            if (genre == null)
+            {
+                //return View("Error");
+                return RedirectToAction("Index");
+            }
+
+            var albums = new List<Albums>();
+
+            for (int i = 1; i < 4; i++)
+            {
+                albums.Add(new Albums { Title = "Album " + i.ToString(), AlbumArtUrl = "/Content/Images/placeholder.gif" });
+            }
+
+            ViewBag.Genre = genre;
+            return View(albums);
+        }
+    }
+}
---
+++ EnterpriseProject/Models/Albums.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EnterpriseProject.Models
+{
+    public class Albums
+    {
+        public string Title { get; set; }
+        public Genre Genre { get; set; }
+        public string AlbumArtUrl { get; set; }
+    }
+}
